b227bc91feb9677a51657af19fc6771d5fa41ba4
eslamsaad0012 <47305568+eslamsaad0012@users.noreply.github.com>
2019-02-13 19:57:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -799,13 +799,21 @@ footer: {
 });
 
 
+var prefix = "#"
 
+client.on("message", message => {
+    if (message.content === (prefix + "help")) {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#580e6b")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`
+   
+   **
+   
 
-client.on('message', message => {
- var prefix = "$";
-if (message.content.startsWith(prefix + 'help')) {
-    let pages = [
-	        
+   **
+   
+ 	         ------------------------------
 		 الاوامر العاديه
          $avatar:
          $id :
@@ -814,51 +822,26 @@ if (message.content.startsWith(prefix + 'help')) {
 		 $topinv:
 		 $invite:
 		 $تقديم:
-	    
-	    
+         ------------------------------
 		 اوامر الادمن
          $clear :
 		 $bc:
 		 $gstart:
-               
-	let page = 1;
 
-    let embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setFooter(`Page ${page} of ${pages.length}`)
-    .setDescription(pages[page-1])
+   **
 
-    message.channel.sendEmbed(embed).then(msg => {
-
-        msg.react('◀').then( r => {
-            msg.react('▶')
-
-
-        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
-        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
-
-
-        const backwards = msg.createReactionCollector(backwardsFilter, { time: 20000});
-        const forwards = msg.createReactionCollector(forwardsFilter, { time: 20000});
-
-
-
-        backwards.on('collect', r => {
-            if (page === 1) return;
-            page--;
-            embed.setDescription(pages[page-1]);
-            embed.setFooter(`Page ${page} of ${pages.length}`);
-            msg.edit(embed)
-        })
-        forwards.on('collect', r => {
-            if (page === pages.length) return;
-            page++;
-            embed.setDescription(pages[page-1]);
-            embed.setFooter(`Page ${page} of ${pages.length}`);
-            msg.edit(embed)
-        })
-        })
-    })
+   `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });  
+client.on('message', message => {
+     if (message.content === (prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#8650a7")
+  .addField("Done" , " تــــم ارســالك في الخــاص")
+  message.channel.sendEmbed(embed);
     }
 });
 
