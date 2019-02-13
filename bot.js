6a56d5b8f19c0e8530b83689d6780d6f6ffc777f
eslamsaad0012 <47305568+eslamsaad0012@users.noreply.github.com>
2019-02-13 21:01:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -791,14 +791,19 @@ footer: {
 
 });
 
-                                   client.on("message", message => {
-                                    var prefix = "$";
-                                 if (message.content === "$help") {
-                                     message.channel.send('');
-                                  const embed = new Discord.RichEmbed() 
-                                      .setColor("RANDOM")
-                                      .setDescription(`
-                                      **
+var prefix = "$"
+
+client.on("message", message => {
+    if (message.content === (prefix + "help")) {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#580e6b")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`
+   
+   **
+   
+
+   **
    
  	         ------------------------------
 		 الاوامر العاديه
@@ -807,17 +812,29 @@ footer: {
          $serverinfo :
 		 $link:
 		 $تقديم:
+		 $new
          ------------------------------
 		 اوامر الادمن
          $clear :
 		 $bc:
 		 $gstart:
+
    **
-                                `)
-                                   message.author.sendEmbed(embed)
-                                    
-                                   }
-                                   }); 
+
+   `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });  
+client.on('message', message => {
+     if (message.content === (prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#8650a7")
+  .addField("Done" , " تــــم ارســالك في الخــاص")
+  message.channel.sendEmbed(embed);
+    }
+});
 
 
 client.login(process.env.BOT_TOKEN);
